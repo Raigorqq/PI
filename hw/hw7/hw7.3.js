c6550c4b53130aa9b6f2b176cb6c2001c34ce5fd
Vadim Tsubin <108688332+Raigorqq@users.noreply.github.com>
2022-11-29 00:05:38 +0200

    Delete hw directory

--- hw/hw7/hw7.3.js
+++
@@ -1,7 +0,0 @@
-var p=document.getElementById("p1").onmouseover = function() {
-    this.style.backgroundColor = "green";
-}
-
-var p=document.getElementById("p1").onmouseout = function() {
-    this.style.backgroundColor = "blue";
-}
